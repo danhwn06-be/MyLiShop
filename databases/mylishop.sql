6b7cffcf57414e52377cdd3caabea08a9620fe3b
danhwn06-be <st.anhdan09@gmail.com>
2025-12-16 03:47:53 +0700

    refactor(assets): restructure images folder and standardize filenames
    chore(database): create categories and products table

---
+++ databases/mylishop.sql
@@ -0,0 +1,20 @@
+-- Active: 1758338882349@@127.0.0.1@3306@mylishop
+CREATE DATABASE IF NOT EXISTS mylishop;
+USE mylishop;
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 0) NOT NULL,
+    description TEXT,
+    image VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
